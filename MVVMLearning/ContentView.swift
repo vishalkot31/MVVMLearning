7d22509c1b61c1d1fda96bf653d29a458458b02d
Vishal Kothari <vishal.chikki@gmail.com>
2025-12-20 17:54:11 +0530

    Initial Commit

---
+++ MVVMLearning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MVVMLearning
+//
+//  Created by Vishal Kothari on 20/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MVVMLearning/MVVMLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMLearningApp.swift
+//  MVVMLearning
+//
+//  Created by Vishal Kothari on 20/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MVVMLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
